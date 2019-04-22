3780e3f0bb811cf077a080c5b37a8d11d6ec0b76
Muhamad Ishlah <nurul.ishlah@qiscus.com>
2019-04-23 06:49:39 +0700

    Add basic structure of plugin

--- qiscus-multichannel-widget.php
+++ qiscus-multichannel-widget.php
@@ -7,3 +7,32 @@
  * Author URI:  https://ishlah.github.io/
  * Plugin URI:  https://github.com/nurulishlah/qiscus-multichannel-widget
 */
+
+/**
+ * Plugin constants
+*/
+
+if (!defined('QMCW_URL'))
+    define('QMCW_URL', plugin_dir_url(__FILE__));
+if (!defined('QMCW_PATH'))
+    define('QMCW_PATH', plugin_dir_path(__FILE__));
+
+/**
+ * Class QiscusMultichannel
+ */
+class QiscusMultichannel
+{
+
+    /**
+     * QiscusMultichannel constructor.
+     */
+    public function __construct()
+    {
+
+    }
+}
+
+/*
+ * Start the plugin
+ */
+new QiscusMultichannel();
