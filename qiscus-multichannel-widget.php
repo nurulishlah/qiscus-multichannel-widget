7e027acfd7c217d97aadc037c544ef3870d59372
Muhamad Ishlah <nurul.ishlah@qiscus.com>
2019-04-23 06:39:53 +0700

    Add plugin header to be recognised as a plugin by WordPress

---
+++ qiscus-multichannel-widget.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Qiscus Multichannel CS Widget
+ * Description: Simple plugin to integrate Qiscus Multichannel CS Widget
+ * Version:     0.0.1
+ * Author:      Muhamad Saad Nurul Ishlah
+ * Author URI:  https://ishlah.github.io/
+ * Plugin URI:  https://github.com/nurulishlah/qiscus-multichannel-widget
+*/
